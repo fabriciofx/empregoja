4c3cc8451ad509b88aac3da03dbc2609c19d2fe9
EvertonTenorio <everton.tenorio10@gmail.com>
2015-09-29 18:39:15 -0300

    Inclusão da classe Usuario (resolve #3)

--- src/main/java/com/github/ifpeppo/empregoja/dominio/Candidato.java
+++ src/main/java/com/github/ifpeppo/empregoja/dominio/Candidato.java
@@ -1,80 +1,68 @@
 package com.github.ifpeppo.empregoja.dominio;
 
 public class Candidato {
+    private String nome;
+    private String cpf;
+    private String celular;
+    private String cidade;
+    private String uf;
+    private Usuario usuario;
+
+    public Candidato(String nome, String cpf, String celular, String cidade, String uf, Usuario usuario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.celular = celular;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.usuario = usuario;
+    }
 	
-	private String nome;
-	private String email;
-	private String cpf;
-	private String celular;
-	private String cidade;
-	private String uf;
-	private String senha;
-
-	public Candidato(String nome, String email, String cpf, String celular,
-			String cidade, String uf, String senha) {
-		this.nome = nome;
-		this.email = email;
-		this.cpf = cpf;
-		this.celular = celular;
-		this.cidade = cidade;
-		this.uf = uf;
-		this.senha = senha;
-	}
+    public String getNome(){
+	return nome;
+    }
 	
-	public String getNome(){
-		return nome;
-	}
+    public void setNome(String nome){
+	this.nome = nome;
+    }
 	
-	public void setNome(String nome){
-		this.nome = nome;
-	}
+    public String getCpf(){
+	return cpf;
+}
 	
-	public String getEmail(){
-		return email;
-	}
-	
-	public void setEmail(String email){
-		this.email = email;
-	}
-	
-	public String getCpf(){
-		return cpf;
-	}
-	
-	public void setCpf(String cpf){
-		this.cpf = cpf;
-	}
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
 
-	public String getCelular() {
-		return celular;
-	}
+    public String getCelular() {
+        return celular;
+    }
 
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
+    public void setCelular(String celular) {
+	this.celular = celular;
+    }
 
-	public String getCidade() {
-		return cidade;
-	}
+    public String getCidade() {
+	return cidade;
+    }
 
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
+    public void setCidade(String cidade) {
+	this.cidade = cidade;
+    }
 
-	public String getUf() {
-		return uf;
-	}
+    public String getUf() {
+	return uf;
+    }
 
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
+    public void setUf(String uf) {
+	this.uf = uf;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public Usuario getUsuario() {
+        return usuario;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}	
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 
 }
