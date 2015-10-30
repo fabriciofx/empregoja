40c0d5dda9addf3af6d76bc47a66b7d300850d4a
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2015-10-30 20:35:54 -0300

    empregoja

---
+++ src/main/java/com/github/ifpeppo/empregoja/controladores/ControladorUsuario.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.ifpeppo.empregoja.controladores;
+
+import com.github.ifpeppo.empregoja.dominio.Candidato;
+import com.github.ifpeppo.empregoja.dominio.repositorio.Candidatos;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author leonardoanselmo
+ */
+public class ControladorUsuario {
+    
+    private static final long serialVersionUID = 1;
+    
+    @Inject
+    private Candidatos candidatos;
+    
+    private Candidato candidato;
+    private List<Candidato> todosCandidatos;
+    
+    public void consultar(){
+        todosCandidatos = candidatos.todos();
+    }
+    
+    public void novo(){
+        candidato = new Candidato();
+    }
+    
+    public String adicianar(){
+        candidatos.adiciona(candidato);
+        consultar();
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Candidato cadastrado com sucesso!"));
+        
+        return "";
+    }
+    
+    public void alterar(){
+        candidatos.alterar(candidato);
+    }
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public List<Candidato> getTodosCandidatos() {
+        return todosCandidatos;
+    }
+    
+    public void detalhe(Candidato candidato){
+        this.candidato = candidato;
+    }
+    
+    
+}
---
+++ src/main/java/com/github/ifpeppo/empregoja/dominio/repositorio/Usuarios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.ifpeppo.empregoja.dominio.repositorio;
+
+
+import com.github.ifpeppo.empregoja.dominio.Usuario;
+import java.util.List;
+
+/**
+ *
+ * @author leonardoanselmo
+ */
+public interface Usuarios extends Repositorio<Usuario, Integer>{
+    
+    public List<Usuario> porNome(String nome);
+    
+    public List<Usuario> porCpf(String Cpf);
+    
+    public void alterar(Usuario usuario);
+    
+}
