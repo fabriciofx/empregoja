52179e4f10a8ccf2f0f1c7338c0899901ee80dbc
EvertonTenorio <everton.tenorio10@gmail.com>
2015-11-30 18:08:10 -0300

    Refatoração de experiencia para Embaddable (resolve #36)

--- src/main/java/com/github/ifpeppo/empregoja/dominio/Candidato.java
+++ src/main/java/com/github/ifpeppo/empregoja/dominio/Candidato.java
@@ -4,12 +4,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "candidato")
@@ -33,7 +38,9 @@ public class Candidato implements Serializable {
     @OneToMany(cascade = CascadeType.ALL)
     private List<Competencia> competencias;
     
-    @OneToMany(cascade = CascadeType.ALL)
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "Experiencia", joinColumns = @JoinColumn(name = "candidato", nullable = false), 
+            uniqueConstraints = @UniqueConstraint(columnNames = {"candidato", "texto" }))
     private List<Experiencia> experiencias;
 
     public Candidato(String nome, String cpf, String celular, Endereco endereco, Usuario usuario, List<Competencia> competencias,
